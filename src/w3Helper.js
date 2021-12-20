90bf4a12ab45c087e6cc72ebb3280baa8b073df7
Colten A Wills <45346350+ctech1313@users.noreply.github.com>
2021-12-20 03:30:58 -0500

    File import

---
+++ src/w3Helper.js
@@ -0,0 +1,13 @@
+const global = {
+    user_profile: {
+        chain: "bsc",
+        entity: "id",
+        saved_tokens: "tokens favorited",
+        balances: "",
+        native_bal: ""
+    },
+
+    helper_data: {
+        bnb_address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+    }
+}
