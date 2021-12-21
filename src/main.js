281d5d13e1137f3b98cba7ebcb9e16c31ecab4c7
Colten A Wills <45346350+ctech1313@users.noreply.github.com>
2021-12-21 04:53:15 -0500

    update

--- src/main.js
+++ src/main.js
@@ -52,98 +52,103 @@ async function setHelperData() {
 
 //JS for VOTE button ONE functionality
 $(function() {
-    $( "#vote_token_1_button" ).click(function() {
-      $( "#vote_token_1_button" ).addClass( "onclic", 250, validate);
+    $("#vote_token_1_button").click(function() {
+        $("#vote_token_1_button").addClass("onclic", 250, validate);
     });
-  
+
     function validate() {
-      setTimeout(function() {
-        $( "#vote_token_1_button" ).removeClass( "onclic" );
-        $( "#vote_token_1_button" ).addClass( "validate", 450, callback );
-      }, 2250 );
-    }
-      function callback() {
         setTimeout(function() {
-          $( "#vote_token_1_button" ).removeClass( "validate" );
-        }, 1250 );
-      }
-    });
+            $("#vote_token_1_button").removeClass("onclic");
+            $("#vote_token_1_button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+    function callback() {
+        setTimeout(function() {
+            $("#vote_token_1_button").removeClass("validate");
+        }, 1250);
+    }
+});
 
 //JS for VOTE button TWO functionality
 $(function() {
-    $( "#vote_token_2_button" ).click(function() {
-      $( "#vote_token_2_button" ).addClass( "onclic", 250, validate);
-    });
-  
-    function validate() {
-      setTimeout(function() {
-        $( "#vote_token_2_button" ).removeClass( "onclic" );
-        $( "#vote_token_2_button" ).addClass( "validate", 450, callback );
-      }, 2250 );
-    }
-      function callback() {
-        setTimeout(function() {
-          $( "#vote_token_2_button" ).removeClass( "validate" );
-        }, 1250 );
-      }
+    $("#vote_token_2_button").click(function() {
+        $("#vote_token_2_button").addClass("onclic", 250, validate);
     });
 
-    //JS for VOTE button THREE functionality
-$(function() {
-    $( "#vote_token_3_button" ).click(function() {
-      $( "#vote_token_3_button" ).addClass( "onclic", 250, validate);
-    });
-  
     function validate() {
-      setTimeout(function() {
-        $( "#vote_token_3_button" ).removeClass( "onclic" );
-        $( "#vote_token_3_button" ).addClass( "validate", 450, callback );
-      }, 2250 );
-    }
-      function callback() {
         setTimeout(function() {
-          $( "#vote_token_3_button" ).removeClass( "validate" );
-        }, 1250 );
-      }
+            $("#vote_token_2_button").removeClass("onclic");
+            $("#vote_token_2_button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+    function callback() {
+        setTimeout(function() {
+            $("#vote_token_2_button").removeClass("validate");
+        }, 1250);
+    }
+});
+
+//JS for VOTE button THREE functionality
+$(function() {
+    $("#vote_token_3_button").click(function() {
+        $("#vote_token_3_button").addClass("onclic", 250, validate);
     });
 
-    //JS for VOTE button FOUR functionality
-$(function() {
-    $( "#vote_token_4_button" ).click(function() {
-      $( "#vote_token_4_button" ).addClass( "onclic", 250, validate);
-    });
-  
     function validate() {
-      setTimeout(function() {
-        $( "#vote_token_4_button" ).removeClass( "onclic" );
-        $( "#vote_token_4_button" ).addClass( "validate", 450, callback );
-      }, 2250 );
-    }
-      function callback() {
         setTimeout(function() {
-          $( "#vote_token_4_button" ).removeClass( "validate" );
-        }, 1250 );
-      }
+            $("#vote_token_3_button").removeClass("onclic");
+            $("#vote_token_3_button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+    function callback() {
+        setTimeout(function() {
+            $("#vote_token_3_button").removeClass("validate");
+        }, 1250);
+    }
+});
+
+//JS for VOTE button FOUR functionality
+$(function() {
+    $("#vote_token_4_button").click(function() {
+        $("#vote_token_4_button").addClass("onclic", 250, validate);
     });
 
-    //JS for VOTE button FIVE functionality
-$(function() {
-    $( "#vote_token_5_button" ).click(function() {
-      $( "#vote_token_5_button" ).addClass( "onclic", 250, validate);
-    });
-  
     function validate() {
-      setTimeout(function() {
-        $( "#vote_token_5_button" ).removeClass( "onclic" );
-        $( "#vote_token_5_button" ).addClass( "validate", 450, callback );
-      }, 2250 );
-    }
-      function callback() {
         setTimeout(function() {
-          $( "#vote_token_5_button" ).removeClass( "validate" );
-        }, 1250 );
-      }
+            $("#vote_token_4_button").removeClass("onclic");
+            $("#vote_token_4_button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+    function callback() {
+        setTimeout(function() {
+            $("#vote_token_4_button").removeClass("validate");
+        }, 1250);
+    }
+});
+
+//JS for VOTE button FIVE functionality
+$(function() {
+    $("#vote_token_5_button").click(function() {
+        $("#vote_token_5_button").addClass("onclic", 250, validate);
     });
+
+    function validate() {
+        setTimeout(function() {
+            $("#vote_token_5_button").removeClass("onclic");
+            $("#vote_token_5_button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+    function callback() {
+        setTimeout(function() {
+            $("#vote_token_5_button").removeClass("validate");
+        }, 1250);
+    }
+});
 
 async function login() {
     try {
@@ -160,11 +165,6 @@ async function login() {
         } else {
             logOut();
         }
-        document.getElementById("vote_token_1_button").disabled = false;
-        document.getElementById("vote_token_2_button").disabled = false;
-        document.getElementById("vote_token_3_button").disabled = false;
-        document.getElementById("vote_token_4_button").disabled = false;
-        document.getElementById("vote_token_5_button").disabled = false;
         document.getElementById("login_button").innerText = "Logout";
         logged_in = true;
     } catch (error) {
@@ -187,22 +187,28 @@ async function logOut() {
 }
 
 async function loginWC() {
-    document.getElementById("login_button_wc").innerText = "Authenticating...";
-    const user = await Moralis.authenticate({ provider: "walletconnect", chainId: 56 });
-    document.getElementById("vote_token_1_button").disabled = false;
-    document.getElementById("vote_token_2_button").disabled = false;
-    document.getElementById("vote_token_3_button").disabled = false;
-    document.getElementById("vote_token_4_button").disabled = false;
-    document.getElementById("vote_token_5_button").disabled = false;
-}
-try {
-
-} catch (error) {
-    if (error.message == "User closed modal") {
-        alert("Login cancelled")
-        document.getElementById("login_button_wc").innerText = "Sign in with WalletConnect";
+    try {
+        currentUser = Moralis.User.current();
+        if (!currentUser) {
+            document.getElementById("login_button_wc").innerText = "Authenticating...";
+            currentUser = await Moralis.authenticate({ provider: "walletconnect", chainId: 56 });
+            document.getElementById("vote_token_1_button").disabled = false;
+            document.getElementById("vote_token_2_button").disabled = false;
+            document.getElementById("vote_token_3_button").disabled = false;
+            document.getElementById("vote_token_4_button").disabled = false;
+            document.getElementById("vote_token_5_button").disabled = false;
+            setHelperData();
+        } else {
+            logOut();
+        }
+        document.getElementById("login_button_wc").innerText = "Logout";
+        logged_in = true;
+    } catch (error) {
+        if (error.message == "User closed modal") {
+            alert("Login cancelled")
+            document.getElementById("login_button_wc").innerText = "Sign in with WalletConnect";
+        }
     }
-
 }
 
 async function logOutWC() {
