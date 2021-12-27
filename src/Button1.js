22dc92964d41c58ef94859182d73c4fe56c6fc78
Colten A Wills <45346350+ctech1313@users.noreply.github.com>
2021-12-27 16:03:20 -0500

    Updated links/ Combine buttons
    Updated links for each token
    Combined all button JS into one file

--- src/Button1.js
+++ src/Button1.js
@@ -3,15 +3,134 @@ var animateButton = function(e) {
     e.preventDefault;
     //reset animation
     e.target.classList.remove('animate');
-    
+
     e.target.classList.add('animate');
-    setTimeout(function(){
-      e.target.classList.remove('animate');
-    },700);
-  };
-  
-  var bubblyButtons = document.getElementsByClassName("bubbly-button1");
-  
-  for (var i = 0; i < bubblyButtons.length; i++) {
+    setTimeout(function() {
+        e.target.classList.remove('animate');
+    }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button1");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
+}
+
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+
+    e.target.classList.add('animate');
+    setTimeout(function() {
+        e.target.classList.remove('animate');
+    }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button2");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
+
+//JS for VOTE button ONE functionality
+$(function() {
+    $("#vote_token_1_button").click(function() {
+        $("#vote_token_1_button").addClass("onclic", 250, validate);
+    });
+
+    function validate() {
+        setTimeout(function() {
+            $("#vote_token_1_button").removeClass("onclic");
+            $("#vote_token_1_button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+    function callback() {
+        setTimeout(function() {
+            $("#vote_token_1_button").removeClass("validate");
+        }, 1250);
+    }
+});
+
+//JS for VOTE button TWO functionality
+$(function() {
+    $("#vote_token_2_button").click(function() {
+        $("#vote_token_2_button").addClass("onclic", 250, validate);
+    });
+
+    function validate() {
+        setTimeout(function() {
+            $("#vote_token_2_button").removeClass("onclic");
+            $("#vote_token_2_button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+    function callback() {
+        setTimeout(function() {
+            $("#vote_token_2_button").removeClass("validate");
+        }, 1250);
+    }
+});
+
+//JS for VOTE button THREE functionality
+$(function() {
+    $("#vote_token_3_button").click(function() {
+        $("#vote_token_3_button").addClass("onclic", 250, validate);
+    });
+
+    function validate() {
+        setTimeout(function() {
+            $("#vote_token_3_button").removeClass("onclic");
+            $("#vote_token_3_button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+    function callback() {
+        setTimeout(function() {
+            $("#vote_token_3_button").removeClass("validate");
+        }, 1250);
+    }
+});
+
+//JS for VOTE button FOUR functionality
+$(function() {
+    $("#vote_token_4_button").click(function() {
+        $("#vote_token_4_button").addClass("onclic", 250, validate);
+    });
+
+    function validate() {
+        setTimeout(function() {
+            $("#vote_token_4_button").removeClass("onclic");
+            $("#vote_token_4_button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+    function callback() {
+        setTimeout(function() {
+            $("#vote_token_4_button").removeClass("validate");
+        }, 1250);
+    }
+});
+
+//JS for VOTE button FIVE functionality
+$(function() {
+    $("#vote_token_5_button").click(function() {
+        $("#vote_token_5_button").addClass("onclic", 250, validate);
+    });
+
+    function validate() {
+        setTimeout(function() {
+            $("#vote_token_5_button").removeClass("onclic");
+            $("#vote_token_5_button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+    function callback() {
+        setTimeout(function() {
+            $("#vote_token_5_button").removeClass("validate");
+        }, 1250);
+    }
+});
